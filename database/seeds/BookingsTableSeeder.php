09463f446004696ec517ea9552f7d5cdea950870
Amelie Le Coz <lecozamelie@hotmail.com>
2020-05-25 08:56:58 +0200

    First Commit

---
+++ database/seeds/BookingsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Bookable;
+use Illuminate\Database\Seeder;
+
+class BookingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Bookable::class, 100)->create();
+    }
+}
