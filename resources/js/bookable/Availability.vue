f567f21d575952bb64cc2a437a0c7ed2b8101dbc
Amelie Le Coz <lecozamelie@hotmail.com>
2020-06-02 22:02:20 +0200

    displaying errors for availability

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -11,20 +11,32 @@
                 placeholder="Start date"
                 v-model="from"
                 @keyup.enter="check"
+                :class="[{'is-invalid': this.errorFor('from')}]"
                 />
             </div>
+            <div
+                class="invalid-feedback"
+                v-for="(error, index) in this.errorFor('from')"
+                :key="'from' + index"
+            >{{ error }}</div>
 
             <div class="form-group col-md-6">
                 <label for="to">To</label>
                 <input 
-                type="text" 
-                name="to" 
-                class="form-control form-control-sm" 
-                placeholder="End date" 
-                v-model="to"
-                @keyup.enter="check"
+                    type="text" 
+                    name="to" 
+                    class="form-control form-control-sm" 
+                    placeholder="End date" 
+                    v-model="to"
+                    @keyup.enter="check"
+                    :class="[{'is-invalid': this.errorFor('to')}]"
                 />
             </div>
+            <div
+                class="invalid-feedback"
+                v-for="(error, index) in this.errorFor('to')"
+                :key="'to' + index"
+            >{{ error }}</div>
         </div>
 
         <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check!</button>
@@ -60,6 +72,20 @@ export default {
                     this.status = error.response.status;
                 })
                 .then(() => (this.loading = false));
+        },
+            errorFor(field) {
+                return this.hasErrors && this.errors[field] ? this.errors[field] : null;
+            }
+    },
+    computed: {
+        hasErrors() {
+            return 422 === this.status && this.errors !== null;
+        },
+        hasAvailability() {
+            return 200 === this.status;
+        },
+        noAvailability() {
+            return 400 === this.status;
         }
     }
 }
@@ -71,5 +97,13 @@ label {
     text-transform: uppercase;
     color: gray;
     font-weight: bolder;
+}
+
+.is-invalid {
+  border-color: #b22222;
+  background-image: none;
+}
+.invalid-feedback {
+  color: #b22222;
 }
 </style> 
